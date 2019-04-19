ca0fbd925bf1726182537b16b678309430bb756e
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-04-19 12:34:18 +0100

    chapter 3 #2

--- ch03.hs
+++ ch03.hs
@@ -1,4 +1,21 @@
 
 --Definition of the grammar
 
-data T = TRUE | FALSE | ITE T T T | ZERO | Succ T | Pred T | Iszero T deriving Show
+--data T = TRUE | FALSE | ITE T T T | ZERO | Succ T | Pred T | Iszero T deriving Show
+data T = TRUE | FALSE | ITE T T T | ZERO | Succ T | Pred T | Iszero T deriving (Eq, Show)
+
+instance Show T where
+	show TRUE 	= "true"
+	show FALSE 	= "false"
+	show (ITE c a b)= "if " ++ (show c) ++ " then " ++ (show a) ++ " else " ++ (show b)
+	show ZERO 	= "0"
+	show (Succ t) 	= "succ " ++ show t
+	show (Pred t) 	= "pred " ++ show t)
+	show (Iszero t) = "iszero " ++ t
+
+iszero :: T -> T
+iszero t = case t of
+	ZERO -> TRUE
+	otherwise -> FALSE
+
+
