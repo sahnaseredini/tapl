6158ae19befd6eecc0bc5dadcb92a3b5344acf0b
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-04-20 20:57:32 +0100

    Chapter 04 is completely done. Test suite implementation is finished just needs some input and output!

--- TestSuite4.hs
+++ TestSuite4.hs
@@ -8,16 +8,18 @@ import Data.Char (intToDigit)
 import Data.List
 import Control.Monad
 
-list = mapM eval allTests
-pairs = zipWithM (,) list allOutputs
+list = fmap eval1 allTests
+pairs = zipWith (,) (liftM show list) (liftM show allOutputs)
 output = foldl (\acc (x, y) -> (x == y) && acc) True pairs 
-outNum = calSteps 1 list allOutputs
+outNum = calSteps 1 (liftM show list) (liftM show allOutputs)
 
 ---calSteps :: Int -> [Term] -> [Term] -> Int
 calSteps step (l:list) (o:output)
-	| null output = if (l == o) then 0 else step
+	| null list = if (l == o) then 0 else step
 	| otherwise = (calSteps (step*2) list output) + (if l == o then 0 else step)
 
+--main = do print list
 main = do print $ (if output == False then show $ elemIndices '1' (reverse $ draw outNum)  else "Works fine!" )
+
 
 draw outNum = showIntAtBase 2 intToDigit outNum ""
