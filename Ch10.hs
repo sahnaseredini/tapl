712520a469b3a4c8ea005c958e4c9faa277513ba
Amir <sahnaseredini@aut.ac.ir>
2019-04-22 17:48:22 +0100

    Chapter10 #2

--- Ch10.hs
+++ Ch10.hs
@@ -6,7 +6,7 @@ type Context = [(String, Binding)]
 
 data Binding = NameBind | VarBind Ty deriving (Show, Eq)
 data Ty = TyBool | TyArr Ty Ty deriving (Show, Eq)
-data Term = TmTrue | TmFalse | TmIf Term Term Term | TmVar Int Int | TmAbs String Ty Term | TmApp Term Term
+data Term = TmTrue | TmFalse | TmIf Term Term Term | TmVar Int Int | TmAbs String Ty Term | TmApp Term Term deriving (Show, Eq)
 
 
 addbinding ctx x bind = ((x, bind) : ctx)
@@ -17,5 +17,11 @@ getTypeFromContext ctx i = case getbinding ctx i of
 				
 getbinding ctx i = snd $ ctx !! i
 
-
+typeOf ctx t = case t of 
+		TmTrue 		-> return TyBool
+		TmFalse 	-> return TyBool
+		TmIf t1 t2 t3 	-> if typeOf ctx t1 == Just TyBool then 
+					if tyt2 == (typeOf ctx t3) then tyt2 
+					else Nothing
+				   else Nothing where tyt2 = typeOf ctx t2
 
