46ccaac511378c1c2d1e13ec76b0c1945d1da411
Amir <sahnaseredini@aut.ac.ir>
2019-04-22 17:40:26 +0100

    Chapter10 #1

--- Ch10.hs
+++ Ch10.hs
@@ -2,6 +2,20 @@ module Ch10 where
 
 import Control.Monad
 
-type Context = [String]
+type Context = [(String, Binding)]
+
+data Binding = NameBind | VarBind Ty deriving (Show, Eq)
+data Ty = TyBool | TyArr Ty Ty deriving (Show, Eq)
+data Term = TmTrue | TmFalse | TmIf Term Term Term | TmVar Int Int | TmAbs String Ty Term | TmApp Term Term
+
+
+addbinding ctx x bind = ((x, bind) : ctx)
+
+getTypeFromContext ctx i = case getbinding ctx i of 
+				VarBind v -> return v
+				otherwise -> Nothing
+				
+getbinding ctx i = snd $ ctx !! i
+
 
 
