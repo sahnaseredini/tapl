da3127e68541e780f71be8ac1293d01413e97739
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-04-23 22:40:05 +0100

    Chapter 11 #3

--- Ch11to14.hs
+++ Ch11to14.hs
@@ -5,8 +5,12 @@ import Control.Monad
 type Context = [(String, Binding)]
 
 data Binding = NameBind | VarBind Ty deriving (Show, Eq)
-data Ty = TyBool | TyArr Ty Ty | TyA | TyUnit | TyPair Ty Ty deriving (Show, Eq)
-data Term = TmTrue | TmFalse | TmIf Term Term Term | TmVar Int Int | TmAbs String Ty Term | TmApp Term Term | TmUnit | TmAs Term Ty | TmLet String Term Term | TmPair Term Term  deriving (Show, Eq)
+data Ty = TyBool | TyArr Ty Ty | TyA | TyUnit | TyPair Ty Ty | TyTuple [Ty] | TyRecord [(String, Ty)] | TySum SumT deriving (Show, Eq)
+data Term = TmTrue | TmFalse | TmIf Term Term Term | TmVar Int Int | TmAbs String Ty Term | TmApp Term Term | TmUnit | TmAs Term Ty | TmLet String Term Term | TmPair Term Term | TmTuple [Term] | TmRecord [(String, Term)] | TmSum Sum deriving (Show, Eq)
+
+data Sum = Inl Term | Inr Term deriving (Show, Eq)
+data SumT = InlT Ty | InrT Ty deriving (Show, Eq)
+data Pattern = Px | PRecord [(String, Pattern)] deriving (Show, Eq)
 
 addbinding :: Context -> String -> Binding -> Context
 addbinding ctx x bind = ((x, bind) : ctx)
@@ -50,4 +54,10 @@ typeOf ctx t = case t of
 		TmPair t1 t2 	-> do tyt1 <- typeOf ctx t1
 				      tyt2 <- typeOf ctx t2
 				      return (TyPair tyt1 tyt2)
+		TmSum s		-> case s of 
+					Inl t1 	-> do tyt1 <- typeOf ctx t1
+						      return (TySum (InlT tyt1))
+					Inr t1 	-> do tyt1 <- typeOf ctx t1
+						      return (TySum (InrT tyt1))
+					_	-> Nothing
 		_ 		-> Nothing
