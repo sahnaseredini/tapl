bb89ea69341fb529323c776dc4ab921d63de00be
Amir <sahnaseredini@aut.ac.ir>
2019-04-28 21:39:05 +0100

    Test suite 11to14 works fine, but needs more input data!

--- InputData11to14.hs
+++ InputData11to14.hs
@@ -3,16 +3,16 @@ module InputData11to14 where
 import Ch11to14
 
 allTests :: [Term]
-allTests = [test0, test1, test2, test3, test4, test5]
+allTests = [test0]
 --allTests = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
 
 test0 = (TmCons [TyBool] TmTrue (TmNil [TyBool]))
-test1 = 
+{-test1 = 
 test2 = 
 test3 = 
 test4 = 
 test5 = 
-{-test6 = 
+test6 = 
 test7 = 
 test8 = 
 test9 = 
