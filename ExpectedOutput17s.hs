29d2c8783bc4a325f9ff5d15d1331d11de5f48f0
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-05-01 10:28:50 +0100

    Input data inserted

--- ExpectedOutput17s.hs
+++ ExpectedOutput17s.hs
@@ -3,7 +3,7 @@ module ExpectedOutput17s where
 import Ch17
 
 allOutputs :: [Maybe Bool]
-allOutputs = [test0, test1, test2, test3, test4]
+allOutputs = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9]
 --allOutputs = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
 
 test0 = Just False
@@ -11,12 +11,12 @@ test1 = Just True
 test2 = Just True
 test3 = Just False
 test4 = Just True
-{-test5 = 
-test6 = 
-test7 = 
-test8 = 
-test9 = 
-test10 = 
+test5 = Just True 
+test6 = Just True
+test7 = Just False
+test8 = Just True
+test9 = Just False
+{-test10 = 
 test11 = 
 test12 = 
 test13 = 
--- InputData17s.hs
+++ InputData17s.hs
@@ -3,7 +3,7 @@ module InputData17s where
 import Ch17
 
 allTests :: [Term]
-allTests = [test0, test1, test2, test3, test4]
+allTests = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9]
 --allTests = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
 
 test0 = ((TyArr TyTop TyTop) (TyArr TyTop (TyRecord [("x", TyTop)])))
@@ -11,12 +11,12 @@ test1 = (TyArr TyTop TyTop) (TyArr TyTop TyTop)
 test2 = (TyArr TyTop TyTop) TyTop
 test3 = TyTop (TyArr TyTop TyTop)
 test4 = TyTop TyTop
-{-test5 = 
-test6 = 
-test7 = 
-test8 = 
-test9 = 
-test10 = 
+test5 = (TyRecord [("x", TyTop)]) (TyRecord [("x", TyTop)])
+test6 = (TyRecord [("x", TyTop)]) (TyRecord [("x", TyTop), ("y", TyTop)])
+test7 = (TyRecord [("x", TyTop), ("y", TyTop)]) (TyRecord [("x", TyTop)])
+test8 = (TyRecord [("x", TyTop), ("y", TyTop)]) (TyRecord [("x", TyTop), ("y", TyTop)])
+test9 = (TyRecord [("x", TyTop), ("y", TyTop)]) (TyRecord [("x", TyTop), ("z", TyTop)])
+{-test10 = 
 test11 = 
 test12 = 
 test13 = 
