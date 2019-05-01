85bd0da54b8d8e32d1f0b772c622d22cfbf56dc1
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-05-01 10:20:35 +0100

    subtype function's implementation is finished

--- Ch17.hs
+++ Ch17.hs
@@ -21,6 +21,10 @@ data Term = TmRecord [(String, Term)]
 subtype :: Ty -> Ty -> Maybe Bool 
 subtype _ TyTop = return True
 subtype (TyArr tyS1 tyS2) (TyArr tyT1 tyT2) = liftM2 (&&) (subtype tyT1 tyS1) (subtype tyS2 tyT2)
+subtype (TyRecord tr1) (TyRecord tr2) = foldl (liftM2 (&&)) (Just True) $ map match tr2 where 
+							match (s, tm2) = case lookup s tr1 of
+										Just tm1 -> subtype tm1 tm2 
+										_ -> return False
 subtype tyS tyT 
 	| tyS == tyT = return True
 	| otherwise = return False
