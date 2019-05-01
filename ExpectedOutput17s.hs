94b53124da0477d630665f673182123b65b015c3
Amir <sahnaseredini@aut.ac.ir>
2019-05-01 10:21:53 +0100

    Test suite files added

---
+++ ExpectedOutput17s.hs
@@ -0,0 +1,28 @@
+module ExpectedOutput17s where
+
+import Ch17
+
+allOutputs :: [Maybe Bool]
+allOutputs = [test0, test1, test2, test3, test4]
+--allOutputs = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
+
+test0 = Just False
+test1 = Just True
+test2 = Just True
+test3 = Just False
+test4 = Just True
+{-test5 = 
+test6 = 
+test7 = 
+test8 = 
+test9 = 
+test10 = 
+test11 = 
+test12 = 
+test13 = 
+test14 = 
+test15 = 
+test16 = 
+test17 = 
+test18 = 
+test19 = -}
