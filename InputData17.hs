1463fa2572c345e41430ee3521ef95b231732705
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-05-01 23:47:26 +0100

    Test suite for Ch17 just added

---
+++ InputData17.hs
@@ -0,0 +1,28 @@
+module InputData17 where
+
+import Ch17
+
+allTests :: [Term]
+allTests = [test0, test1, test2]
+--allTests = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
+
+test0 = (TmProj (TmVar 0 1) "x")
+test1 = (TmProj (TmRecord [("x", (TmVar 0 0))]) "x")
+test2 = (TmProj (TmRecord [("x", (TmVar 0 0))]) "y")
+{-test3 = 
+test4 = 
+test5 = 
+test6 = 
+test7 = 
+test8 = 
+test9 = 
+test10 = 
+test11 = 
+test12 = 
+test13 = 
+test14 = 
+test15 = 
+test16 = 
+test17 = 
+test18 = 
+test19 = -}
---
+++ TestSuite17.hs
@@ -0,0 +1,25 @@
+module TestSuite17 where
+
+import Ch17
+import InputData17
+import ExpectedOutput17
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
+import Data.List
+import Control.Monad
+
+list = fmap (typeOf [("x", VarBind TyTop)]) allTests
+pairs = zipWith (,) (liftM show list) (liftM show allOutputs)
+output = foldl (\acc (x, y) -> (x == y) && acc) True pairs 
+outNum = calSteps 1 (liftM show list) (liftM show allOutputs)
+
+---calSteps :: Int -> [Term] -> [Term] -> Int
+calSteps step (l:list) (o:output)
+	| null list = if (l == o) then 0 else step
+	| otherwise = (calSteps (step*2) list output) + (if l == o then 0 else step)
+
+--main = do print list
+main = do print $ (if output == False then show $ elemIndices '1' (reverse $ draw outNum)  else "Works fine!" )
+
+
+draw outNum = showIntAtBase 2 intToDigit outNum ""
