29581424bc8920995fb81737e3ed57bcbeabf0d3
sahnaseredini <sahnaseredini@aut.ac.ir>
2019-05-06 12:53:52 +0100

    Ch17 test suite is done

--- ExpectedOutput17.hs
+++ ExpectedOutput17.hs
@@ -3,14 +3,14 @@ module ExpectedOutput17 where
 import Ch17
 
 allOutputs :: [Maybe Ty]
-allOutputs = [test0, test1, test2]
+allOutputs = [test0, test1, test2, test3]
 --allOutputs = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
 
 test0 = Nothing
 test1 = Just TyTop
 test2 = Nothing
-{-test3 = 
-test4 = 
+test3 = Just TyTop
+{-test4 = 
 test5 = 
 test6 = 
 test7 = 
--- InputData17.hs
+++ InputData17.hs
@@ -3,14 +3,14 @@ module InputData17 where
 import Ch17
 
 allTests :: [Term]
-allTests = [test0, test1, test2]
+allTests = [test0, test1, test2, test3]
 --allTests = [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18, test19]
 
 test0 = (TmProj (TmVar 0 1) "x")
 test1 = (TmProj (TmRecord [("x", (TmVar 0 0))]) "x")
 test2 = (TmProj (TmRecord [("x", (TmVar 0 0))]) "y")
-{-test3 = 
-test4 = 
+test3 = (TmApp (TmAbs "x" TyTop (TmVar 0 1)) (TmVar 0 1))
+{-test4 = 
 test5 = 
 test6 = 
 test7 = 
